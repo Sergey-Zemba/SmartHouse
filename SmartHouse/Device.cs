89324e26f93fb861010d527b66e52027b2543501
Sergey-Zemba <sergey.zemba@gmail.com>
2015-09-23 18:56:58 +0300

    Adding Smart House

---
+++ SmartHouse/Device.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse
+{
+    abstract class Device : ISwitchable
+    {
+    {
+        public SwitchState State { get; set; }
+
+        public Device()
+        {
+
+        }
+        public void On()
+        {
+            State = SwitchState.On;
+        }
+
+        public void Off()
+        {
+            State = SwitchState.Off;
+        }
+    }
+}
---
+++ SmartHouse/Window.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse
+{
+    class Window : IOpenable
+    {
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
